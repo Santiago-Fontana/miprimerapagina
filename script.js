0b94db8390ec7b0a82258dab340523edb37acdf9
Santiago-Fontana <alsurdelatardecer@gmail.com>
2023-06-14 22:15:08 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showalert() { alert("ALERTA!!!"); }
+let boton = document.getElementById("miboton");
+boton.addEventListener("click", function() {
+    alert("haz hecho click en el boton");
+});
+
